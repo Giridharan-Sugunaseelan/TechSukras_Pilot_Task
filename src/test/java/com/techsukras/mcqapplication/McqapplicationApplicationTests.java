84ae61ff5776151c97d7ed1f4ebe4734ed9dc45a
Giridharan <slbg.giridharan@gmail.com>
2024-07-30 20:25:35 +0530

    Initial Commit

---
+++ src/test/java/com/techsukras/mcqapplication/McqapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsukras.mcqapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McqapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
